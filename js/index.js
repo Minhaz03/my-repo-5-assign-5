677d7bb60656f875666741a32a1882d32ab112a3
Minhaz03 <minhazulalamsohan2002@gmail.com>
2024-09-23 15:44:52 +0600

    blog html updated

--- js/index.js
+++ js/index.js
@@ -2,7 +2,11 @@ const btnDonate = document.getElementById('btn-card-1');
 btnDonate.addEventListener('click', function(){
     // console.log('testing')
     const donationAmount = parseFloat(document.getElementById('donation-input').value);
-    console.log(donationAmount)
+    // console.log(donationAmount)
+    // validation for input donation
+    if (donationAmount < 0){
+        return alert('Try Valid Number')
+    }
     
     const prevAmount = parseFloat(document.getElementById('donated-money').innerText);
     // console.log(prevAmount)
@@ -14,6 +18,10 @@ btnDonate.addEventListener('click', function(){
     const mainBalance = parseFloat(document.getElementById('main-balance').innerText);
 
     const newMainBalance = mainBalance - donationAmount;
+    // validation for new main balance in top right
+    if (newMainBalance < 0){
+        return alert('Something Wrong Please Try Again Later')
+    }
     // console.log(newMainBalance)
     document.getElementById('main-balance').innerText = newMainBalance;
 });
