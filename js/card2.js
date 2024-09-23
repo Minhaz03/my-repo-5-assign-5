83d3687d981ab894ed2b04e98a0afb13a3dac6b3
Minhaz03 <minhazulalamsohan2002@gmail.com>
2024-09-23 17:58:42 +0600

    modal added in donate btn

---
+++ js/card2.js
@@ -0,0 +1,21 @@
+const btnDonateCard2 = document.getElementById('btn-card-2');
+btnDonateCard2.addEventListener('click', function(){
+    // console.log('connected')
+    const mainBalance = getTextFieldValueById('main-balance');
+    const donateInput = getInputFieldValueById('input-btn-card-2');
+    const donationCollected = getTextFieldValueById('donated-money-added');
+    if (donateInput < 0){
+        return alert('Try Valid Number')
+    }
+    // console.log(donateInput, donationCollected)
+
+    const newDonatedCollected = donationCollected + donateInput;
+    // console.log(newDonatedCollected)
+    document.getElementById('donated-money-added').innerText = newDonatedCollected;
+
+    const newBalance = mainBalance - donateInput;
+    if(newBalance < 0){
+        return alert('Something Wrong Please Try Again Later')
+    }
+    document.getElementById('main-balance').innerText = newBalance;
+});
