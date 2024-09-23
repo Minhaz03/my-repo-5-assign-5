9393cdfb314298c8e2c178757c82302f6f48c6a5
Minhaz03 <minhazulalamsohan2002@gmail.com>
2024-09-23 11:05:54 +0600

    blog js added again

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,5 @@
 document.getElementById('btn-blog').addEventListener('click', function(){
     // console.log('connected');
 
-    window.location.href = '/blog.html';
+    window.location.href = '/my-repo-5-assign-5/blog.html';
 });
